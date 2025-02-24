f5696d32222889cbc0cdbdd23fc6d51b625c489a
NVDespotov20 <nvdespotov20@codingburgas.bg>
2025-02-24 12:38:45 +0200

    Add migration

---
+++ src/FlowGuardMonitoring.DAL/Migrations/20250224081130_RefactorMeasurementEntity.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlowGuardMonitoring.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorMeasurementEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Contaminants",
+                table: "Measurements");
+
+            migrationBuilder.DropColumn(
+                name: "QualityIndex",
+                table: "Measurements");
+
+            migrationBuilder.DropColumn(
+                name: "Temperature",
+                table: "Measurements");
+
+            migrationBuilder.DropColumn(
+                name: "WaterLevel",
+                table: "Measurements");
+
+            migrationBuilder.DropColumn(
+                name: "pH",
+                table: "Measurements");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Value",
+                table: "Measurements",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: string.Empty);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Value",
+                table: "Measurements");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Contaminants",
+                table: "Measurements",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "QualityIndex",
+                table: "Measurements",
+                type: "real",
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Temperature",
+                table: "Measurements",
+                type: "real",
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "WaterLevel",
+                table: "Measurements",
+                type: "real",
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "pH",
+                table: "Measurements",
+                type: "real",
+                nullable: true);
+        }
+    }
+}
